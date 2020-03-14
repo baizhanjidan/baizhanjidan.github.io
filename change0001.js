6a932a0b00f7d721d61336cf07b5fe899443e59f
snoe <16965118+baizhanjidan@users.noreply.github.com>
2020-03-14 21:23:23 +0800

    Create change0001.js

---
+++ change0001.js
@@ -0,0 +1,10 @@
+window.onload=function(){        
+var Imgbtn=document.getElementById('btn');        
+var Img=document.getElementById('img');        
+Imgbtn.onclick=function(){            
+if(Img.src=='https://i2.tiimg.com/700213/48ef680778032a92.jpg')            
+{             
+Img.src='http://i1.fuimg.com/663097/72b064ae2f864822.jpg';          
+}else{                    
+Img.src='https://i2.tiimg.com/700213/48ef680778032a92.jpg'  
+}}}    
