98e290b4c5a0a9aa32f172fd69168abf18e16630
snoe <16965118+baizhanjidan@users.noreply.github.com>
2020-03-15 23:57:05 +0800

    Update 031504.js

--- 031504.js
+++ 031504.js
@@ -8,9 +8,16 @@ $(document).ready(function () {
     
     $("#ceshi5").on("click", function() {
       var imglist = [
-        "https://i.loli.net/2020/03/15/VmQhgoJNkbYRfA4.gif",
-        "https://i.loli.net/2020/03/15/yFlAeYrPI6zxZ5t.png",
-
+        "http://i1.fuimg.com/712728/65bab655787573cf.png",
+        "http://i1.fuimg.com/712728/fe491bac039dc489.png",
+	"http://i1.fuimg.com/712728/063495cb8f04041a.png",
+	"http://i1.fuimg.com/712728/263e93258920e480.png",
+	"http://i1.fuimg.com/712728/ca47402e66770c6e.png",
+	"http://i1.fuimg.com/712728/d026c0d5ea67a935.png",
+	"http://i1.fuimg.com/712728/13419aacc651c63b.png",
+	"http://i1.fuimg.com/712728/5415255332a70011.png",
+	"http://i1.fuimg.com/712728/8430c58f7c02bb7e.png".
+	"http://i1.fuimg.com/712728/403eb02ecf19a45f.png"
       ];
       $("#ceshi2").empty();
       for (var b = 0; b < imglist.length; b++) {
