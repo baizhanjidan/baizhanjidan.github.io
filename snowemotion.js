1ebf86f1bd71f281c00a396f96e287db07f8b16f
snoe <16965118+baizhanjidan@users.noreply.github.com>
2020-03-28 20:23:11 +0800

    Update snowemotion.js

--- snowemotion.js
+++ snowemotion.js
@@ -4,8 +4,19 @@ $(document).ready(function () {
 	$("div.text-success").before("<div align='center' id='snow'></div>");
     $("#snow").html(snow_lp);
     $("#acn").before('<div class="am-tab-panel am-fade" id="snow01"></div>');
-    $(".am-tabs-nav.am-nav.am-nav-tabs").prepend("<li id='snow02' class><a href='#snow01'>吉祥三豹</li>");
-    
+    $(".am-tabs-nav.am-nav.am-nav-tabs").prepend("<li id='snow02' class><a href='#snow01'>俺的bqb</li>");
+    $("#shanchu").on("click", function () {
+        $("#wenbenkuang").val($("#wenbenkuang").val() + '<s></s>');
+    });
+    $("#zhedie").on("click", function () {
+        $("#wenbenkuang").val($("#wenbenkuang").val() + '<details><summary>Click to open</summary>内容</details>');
+    });
+    $("#lianjie").on("click", function () {
+        $("#wenbenkuang").val($("#wenbenkuang").val() + '<textarea class="form-control">文字</textarea>');
+    });
+    $("#wenzi").on("click", function () {
+        $("#wenbenkuang").val($("#wenbenkuang").val() + '<a href="地址" target="_blank">点就看</a>');
+    });
     $("#snow02").on("click", function() {
       var imglist = [
         "http://i1.fuimg.com/712728/65bab655787573cf.png",
@@ -18,7 +29,6 @@ $(document).ready(function () {
 	"http://i1.fuimg.com/712728/5415255332a70011.png",
         "http://i1.fuimg.com/712728/8430c58f7c02bb7e.png",
         "http://i1.fuimg.com/712728/403eb02ecf19a45f.png",
-
       ];
       $("#snow01").empty();
       for (var b = 0; b < imglist.length; b++) {
@@ -31,8 +41,36 @@ $(document).ready(function () {
         $("#snow01").append(snowemo);
       }
       $("#snow01").append(
-        '<br>'
+        '<br>常用代码:<br><font id="shanchu" color="#4682B4">删除线</font>&nbsp;&nbsp;<font id="zhedie" color="#4682B4">折叠</font>&nbsp;&nbsp;<font id="lianjie" color="#4682B4">点就看</font>&nbsp;&nbsp;<font id="wenzi" color="#4682B4">文字框</font>'
       );
+      $("#shanchu").on("click", function() {
+        $("#wenbenkuang").val(
+          $("#wenbenkuang").val() +
+            '<s></s>'
+        );
+        $(".am-tabs-bd").hide();
+      });
+      $("#zhedie").on("click", function() {
+        $("#wenbenkuang").val(
+          $("#wenbenkuang").val() +
+            '<details><summary>Click to open</summary>内容</details>'
+        );
+        $(".am-tabs-bd").hide();
+      });
+      $("#lianjie").on("click", function() {
+        $("#wenbenkuang").val(
+          $("#wenbenkuang").val() +
+            '<a href="地址" target="_blank">点就看</a>'
+        );
+        $(".am-tabs-bd").hide();
+      });
+      $("#wenzi").on("click", function() {
+        $("#wenbenkuang").val(
+          $("#wenbenkuang").val() +
+            '<textarea class="form-control">文字</textarea>'
+        );
+        $(".am-tabs-bd").hide();
+      });
       $(".am-tabs-bd").show();
     });
 });
