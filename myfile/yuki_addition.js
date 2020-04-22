95bac74fb009b0faf1dc789b91d29e9b5bde39f6
snoe <16965118+baizhanjidan@users.noreply.github.com>
2020-04-22 12:26:39 +0800

    Update yuki_addition.js

--- myfile/yuki_addition.js
+++ myfile/yuki_addition.js
@@ -1,1 +1,1 @@
-$("#contentdiv").prepend("<link rel="stylesheet" href='https://baizhanjidan.github.io/myfile/yuki_addition.css'>")
+$("#p2473771").prepend("<link rel="stylesheet" href='https://baizhanjidan.github.io/myfile/yuki_addition.css'>")
