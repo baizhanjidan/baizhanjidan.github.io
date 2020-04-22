3ffc9891dae1634f1a97773e7aa8813422828846
snoe <16965118+baizhanjidan@users.noreply.github.com>
2020-04-22 12:55:55 +0800

    Update yuki_addition.js

--- myfile/yuki_addition.js
+++ myfile/yuki_addition.js
@@ -1,1 +1,1 @@
-$("#p2478561").prepend('<link rel="stylesheet" href="https://baizhanjidan.github.io/myfile/yuki_addition.css"><script id="myhk" src="https://home.lmih.cn/player/js/player.js" key="158361722577" m="1"></script>')
+$("#contentdiv").prepend('<link rel="stylesheet" href="https://baizhanjidan.github.io/myfile/yuki_addition.css"><script id="myhk" src="https://home.lmih.cn/player/js/player.js" key="158361722577" m="1"></script>')
