8e3959fba94016dd81a5fc7745bee6737c0b2509
snoe <16965118+baizhanjidan@users.noreply.github.com>
2020-04-24 20:39:08 +0800

    Create yuki_changeimg.js

---
+++ myfile/yuki_changeimg.js
@@ -0,0 +1,3 @@
+$("#p2444321").each(function(){
+$(this).replaceWith('<div class="replybody"><img src="https://i.loli.net/2020/04/24/KrAnmfZF3pDUtYV.png"><br>雪雪放大后的精二头像可以明显地看到小虎牙哦！<br><img src="https://i.loli.net/2020/04/24/JFUKoQSgIDL6YO7.png"><br>众所周知崽崽的小虎牙也是很明显的！<br>那么……<br>在这样强势的豹家基因下阿银你是不是也……<img src="https://i2.tiimg.com/700213/48ef680778032a92.jpg" class="emotion"><br></div><div class="authorinfo">№121&nbsp;☆☆☆= =于2020-03-17 13:39:50留言☆☆☆<hr style="margin-top: 2px;"></div>');
+});
