7335efbc9c579529766d2aa086e819fd2efc09d6
snoe <16965118+baizhanjidan@users.noreply.github.com>
2020-04-24 21:32:16 +0800

    Update yuki_changeimg.js

--- myfile/yuki_changeimg.js
+++ myfile/yuki_changeimg.js
@@ -1,3 +1,8 @@
 $("#p2444321").each(function(){
-$(this).replaceWith('<div class="replybody"><img src="https://i.loli.net/2020/04/24/KrAnmfZF3pDUtYV.png"><br>雪雪放大后的精二头像可以明显地看到小虎牙哦！<br><img src="https://i.loli.net/2020/04/24/JFUKoQSgIDL6YO7.png"><br>众所周知崽崽的小虎牙也是很明显的！<br>那么……<br>在这样强势的豹家基因下阿银你是不是也……<img src="https://i2.tiimg.com/700213/48ef680778032a92.jpg" class="emotion"><br></div><div class="authorinfo">№121&nbsp;☆☆☆= =于2020-03-17 13:39:50留言☆☆☆<hr style="margin-top: 2px;"></div>');
+$(this).replaceWith('<div class="replybody"><img src="https://i.loli.net/2020/04/24/KrAnmfZF3pDUtYV.png"><br>雪雪放大后的精二头像可以明显地看到小虎牙哦！<br><img src="https://i.loli.net/2020/04/24/JFUKoQSgIDL6YO7.png"><br>众所周知崽崽的小虎牙也是很明显的！<br>那么……<br>在这样强势的豹家基因下阿银你是不是也……<img src="https://i2.tiimg.com/700213/48ef680778032a92.jpg" class="emotion"><br></div><div class="authorinfo"><div>№121&nbsp;☆☆☆<span class="group_0">= =</span> 于<span style="color:#393D49"> 2020-03-17 13:39:50</span>留言☆☆☆</div><hr style="margin-top: 2px;"></div>');
+});
+
+
+$("#p2460033").each(function(){
+$(this).replaceWith('<div class="replybody"><font color="#777777">高台与近战的不 可 兼 容<br>就如同雪雪与阿银的形同陌路一般<br>既可高台亦可近战的崽崽夹在哥哥姐姐之间举 步 维 艰<br>这就是命中注定的咫尺天涯吗<br>其实我和豹豹们过了低保的（小声<br>№125☆☆☆&nbsp;= = 于 2020-03-25 01:15:26留言☆☆☆</font><br>虽然这么说了，但是豹豹的潜力是不容小觑的！<br><img src="https://i.loli.net/2020/04/24/BHJZdn1VCjqcWkY.png" width="300"><br>这次是借好友的专三崽崽打的，手感真好<img src="https://i.postimg.cc/KjBmRcnk/37.png" class="emotion">昨天用自己的未专精崽崽和一技能雪雪也过惹！我很菜所以只打到21，理论上22可行，盾和工具人练度再高一点的话能点三级虫三级斧，手速够快2425不是梦<img src="https://s2.ax1x.com/2019/12/24/lCzjl4.png" class="emotion"><br>削弱+输出+控制，豹豹们真是相性绝佳呢<img src="https://s2.ax1x.com/2019/12/05/Q3yJEQ.gif" class="emotion"><s>只不过从人设上来说是另一回事</s></div><div class="authorinfo"><div>№129&nbsp;☆☆☆<span class="group_0">= =</span> 于<span style="color:#393D49"> 2020-03-29 22:09:24</span>留言☆☆☆</div><hr style="margin-top: 2px;"></div>');
 });
